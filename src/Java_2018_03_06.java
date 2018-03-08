6e150e08e616bf11cd618cd8c0c8be6e5f022bd6
KID <hjh010501@naver.com>
2018-03-08 09:36:13 +0900

    2018_03_08

---
+++ src/Java_2018_03_06.java
@@ -0,0 +1,20 @@
+public class Java_2018_03_06 {
+
+    public static void main(String[] args) {
+
+        int i;
+        double r;
+
+        r = 5/4;
+        System.out.println(r);
+        r = (double)5/4;
+        System.out.println(r);
+        r = 5 / (double)4;
+        System.out.println(r);
+        r = (double)5 / (double)4;
+        System.out.println(r);
+        i = (int)1.3 + (int) 1.9;
+        System.out.println(i);
+
+    }
+}
---
+++ src/Java_2018_03_08.java
@@ -0,0 +1,45 @@
+///**
+// * Created by KID on 2018. 3. 8..
+// */
+//public class Java_2018_03_08 {
+//
+//    public static void main(String[] args) {
+//
+//        String name = "함종현";
+//        int age = 18;
+//        String gender = "Male";
+//        double key = 178;
+//        float kg = 70;
+//        String school = "Korea Digital Media HighSchool";
+//        String awdawd = "Web Programming";
+//        long birth = 20010501;
+//
+//        System.out.println(name);
+//        System.out.println(age);
+//        System.out.println(gender);
+//        System.out.println(key);
+//        System.out.println(kg);
+//        System.out.println(school);
+//        System.out.println(awdawd);
+//        System.out.println(birth);
+//
+//    }
+//
+//}
+
+
+public class Java_2018_03_08 {
+
+    public static void main(String[] args) {
+
+        int distance;
+        float speed = 90.0f;
+        float time = 60.0f;
+
+        distance = (int)(speed*time);
+        System.out.println(distance);
+
+    }
+
+}
+
